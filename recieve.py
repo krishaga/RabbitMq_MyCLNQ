644c8cb4b46e2f5000d42d51884f0db2a86fe694
Krish <krishagarwal1673@gmail.com>
2025-07-10 19:07:01 +0530

    Understand work Queues

--- recieve.py
+++ recieve.py
@@ -1,25 +1,20 @@
-import pika, sys, os
+import pika, time
 
-def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+channel.queue_declare(queue='task_queue', durable=True)
+print(' [*] Waiting for messages. To exit press CTRL+C')
 
-    def callback(ch, method, properties, body):
-        print(f" [x] Received {body}")
+def callback(ch, method, properties, body):
+    print(f" [x] Received {body.decode()}")
+    time.sleep(body.count(b'.'))
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='task_queue', on_message_callback=callback, auto_ack=True)
 
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+channel.start_consuming()
+
--- send.py
+++ send.py
@@ -1,14 +1,18 @@
-import pika
+import pika, sys
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='Hello')
+channel.queue_declare(queue='task_queue', durable=True)
+
+message = '  '.join(sys.argv[1:])
 
 channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')
+                      routing_key='task_queue',
+                      body=message,
+                      properties=pika.BasicProperties(
+                          delivery_mode=pika.DeliveryMode.Persistent
+                      ))
 
-print(" [X] Sent 'Hello World!'")
-
+print(f"[X] Sent {message}")
 connection.close()
